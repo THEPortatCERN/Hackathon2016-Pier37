62f18fd46afb98cfab118ec6d51599039387f9d9
Dan Lemon <daniel.lemon@amazee.com>
2016-10-15 17:00:14 +0200

    Test flickr images

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
+(function(document, window, $, config) {
+  'use strict';
 
+  var apiURL = 'https://api.flickr.com/services/rest/';
+
+  $.getJSON("http://api.flickr.com/services/feeds/groups_pool.gne?id=" + config.apiKey + "@N22&lang=en-us&format=json&jsoncallback=?", function(data) {
+    $.each(data.items, function(i, item){
+      $("<img/>").attr("src", item.media.m).appendTo("#images")
+      .wrap("<a href='" + item.link + "'></a>");
+    });
+  });
+})(document, window, jQuery, config);
