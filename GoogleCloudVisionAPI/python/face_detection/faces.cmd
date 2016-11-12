1aa777db34c5a331633ad92883038968ba29d736
Pragyan <psorisha@gmail.com>
2016-11-12 18:33:00 -0500

    References from ICRC and Google Cloud Vision API code for post-processing label, auto-annotate and face detection

---
+++ GoogleCloudVisionAPI/python/face_detection/faces.cmd
@@ -0,0 +1,4 @@
+faces.py .\resources\img1.jpeg
+move out.jpg img1_out.jpg
+faces.py .\resources\1_original.jpg 
+move out.jpg 1_original_out.jpg
---
+++ GoogleCloudVisionAPI/python/label/label.cmd
@@ -0,0 +1,3 @@
+label.py .\resources\img1.jpeg > img1_label.txt
+label.py .\resources\1_original.jpg > 1_original_label.txt
+label.py .\resources\cat.jpg > cat_label.txt
